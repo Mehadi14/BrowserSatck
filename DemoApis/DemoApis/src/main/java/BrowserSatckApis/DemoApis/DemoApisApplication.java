38466def74daaa364fdcbd0b93b7c94a24e24d1b
Mehadi14 <83648005+Mehadi14@users.noreply.github.com>
2023-07-16 21:53:58 +0530

    Add files via upload

---
+++ DemoApis/DemoApis/src/main/java/BrowserSatckApis/DemoApis/DemoApisApplication.java
@@ -0,0 +1,13 @@
+package BrowserSatckApis.DemoApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApisApplication.class, args);
+	}
+
+}
